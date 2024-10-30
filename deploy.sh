69dca2ab6076e30926ed2bbb05a0a7df56b34bbf
sooarin <minjikim@wavve.com>
2024-10-30 19:19:55 +0900

    feat: 배포 shell 수정

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@
          sleep 3
  fi
 
- JAR_PATH="/home/ec2-user/cicd/*.jar"
+ JAR_PATH="/home/deploy-user/cicd/*.jar"
  echo "jar path : $JAR_PATH"
  chmod +x $JAR_PATH
- nohup java -jar $JAR_PATH >> /home/ec2-user/cicd/deploy.log 2>> /home/ec2-user/cicd/deploy_err.log &
+ nohup java -jar $JAR_PATH >> /home/deploy-user/cicd/deploy.log 2>> /home/deploy-user/cicd/deploy_err.log &
  echo "jar fild deploy success"
