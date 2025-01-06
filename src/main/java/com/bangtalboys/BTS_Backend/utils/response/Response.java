57757d35e8ba28fa3d482308ace692ed55115bc9
sooarin <minjikim@wavve.com>
2025-01-06 14:38:46 +0900

    feat: response.java에 누락된 getter 추가

--- src/main/java/com/bangtalboys/BTS_Backend/utils/response/Response.java
+++ src/main/java/com/bangtalboys/BTS_Backend/utils/response/Response.java
@@ -2,7 +2,7 @@ package com.bangtalboys.BTS_Backend.utils.response;
 
 import lombok.Getter;
 import lombok.Setter;
-
+@Getter
 public class Response<T> {
     private String code;
     private String message;
