dac0da92cf6a3a6069a2076623682a15a9a0be96
duddud0108 <duddud0108@wavve.com>
2025-01-09 10:15:12 +0900

    feat: jwt 인터셉터 제거

--- src/main/java/com/bangtalboys/BTS_Backend/config/WebMvcConfig.java
+++ src/main/java/com/bangtalboys/BTS_Backend/config/WebMvcConfig.java
@@ -1,19 +1,17 @@
 package com.bangtalboys.BTS_Backend.config;
 
-import com.bangtalboys.BTS_Backend.interceptor.JwtInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private final JwtInterceptor jwtTokenInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        System.out.println("인터셉터 등록");
-        registry.addInterceptor(jwtTokenInterceptor).addPathPatterns("/**").excludePathPatterns("/api/auth/kakao");
-    }
+//    private final JwtInterceptor jwtTokenInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        System.out.println("인터셉터 등록");
+//        registry.addInterceptor(jwtTokenInterceptor).addPathPatterns("/**").excludePathPatterns("/api/auth/kakao");
+//    }
 }
