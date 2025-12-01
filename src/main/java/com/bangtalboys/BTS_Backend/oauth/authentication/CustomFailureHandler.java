50a2b86d74ed66b0110fb3121904b5f031441d18
duddud0108 <duddud0108@wavve.com>
2025-12-01 15:35:47 +0900

    소셜로그인 실패 로그 추가

--- src/main/java/com/bangtalboys/BTS_Backend/oauth/authentication/CustomFailureHandler.java
+++ src/main/java/com/bangtalboys/BTS_Backend/oauth/authentication/CustomFailureHandler.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 
 @Component
@@ -15,6 +14,7 @@ public class CustomFailureHandler implements AuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request,
                                         HttpServletResponse response,
                                         AuthenticationException exception) throws IOException {
+        System.out.println("🔥 인증 실패: " + exception.getMessage());
         // 실패 시 리다이렉트 URL 지정
         response.sendRedirect("http://localhost:3000/oauth/login?result=error");
     }
